4282bf3bf85e22026f1a13eaf65e7729e8e62dab
Emin <makhmudovemin441@gmail.com>
2024-09-12 15:38:34 +0500

    buttons without functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from datetime import date
 import os
 from dotenv import load_dotenv
@@ -15,7 +16,18 @@ def start(message):
     my_message = f'Здравствуйте, <b>Dr.</b><b>{message.from_user.first_name}</b>'
     bot.send_message(message.chat.id, my_message, parse_mode='html')
     
+    markup = types.ReplyKeyboardMarkup()
     
+    name_list = types.KeyboardButton('Получить список пациентов за сегодня')
+    name_input = types.KeyboardButton('Ввести пациента')
+    days_list = types.KeyboardButton('Получить список за каждый день недели')
+    
+    markup.row(name_input, days_list)
+    markup.row(name_list)
+    
+    bot.send_message(message.chat.id,'Держите список функций!', reply_markup=markup)
+
+
 
 bot.polling(non_stop=True)
 
